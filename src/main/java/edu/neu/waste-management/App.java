5ad733118391cfddb8e6d5fb8aac5d15671ee018
Raveena Ingale <ingale.r@northeastern.edu>
2023-12-04 22:50:37 -0500

    chore: initial project setup

---
+++ src/main/java/edu/neu/waste-management/App.java
@@ -0,0 +1,13 @@
+package edu.neu.waste-management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

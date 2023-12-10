611c7f59116a054ae1943fa6a74e406cf49d2e04
Prarthana Shetty <shetty.prar@northeastern.edu>
2023-12-10 00:08:04 -0500

    Product integration

--- src/main/java/edu/neu/wasteManagement/business/products/ProductCatalog.java
+++ src/main/java/edu/neu/wasteManagement/business/products/ProductCatalog.java
@@ -1,13 +1,28 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+* Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+* Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+*/
 package edu.neu.wasteManagement.business.products;
-
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
 /**
- *
- * @author ingale.r
- */
+*
+* @author prart
+*/
 public class ProductCatalog {
-    
+    private List<Product> products;
+ 
+    public ProductCatalog() {
+        this.products = new ArrayList<>();
+    }
+ 
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+ 
+    public List<Product> getProducts() {
+        return products;
+    }
 }
